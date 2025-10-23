6c86f87831c55d493c277310b5ab81b8833145ea
JAINISH-CHAUHAN <jainish040405@gmail.com>
2025-10-23 23:04:52 +0530

    FASTAPI Setup

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+items = []
+
+@app.get('/')
+def root():
+    return {'Hello':'World'}
+
+@app.post('/items')
+def create_items(item: str):
+    items.append(item)
+    return items
+
+@app.get('/items/{item_id}')
+def get_item(item_id : int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.get('items/')
+def list_items(limit : int = 10):
+    return items[0:limit]
